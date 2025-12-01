419ff54d18ed9c69ab0efa385e3fba391a9cad7e
ysofer18-cpu <ysofer18@gmail.com>
2025-12-01 09:17:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,16 @@
-const CACHE_NAME = 'dropnow-routeflow-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest'
-];
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open('dropnow-cache-v1').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.webmanifest',
+      './assets/logo.jpg'
+    ]))
   );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-});
-
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
